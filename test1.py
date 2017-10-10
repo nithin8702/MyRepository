fbfa32be1f8342c0b06c0a62ce17ccd873c9d99c
nithin8702 <nithin8702@gmail.com>
2017-10-10 06:55:46 +0530

    .
    .

--- test1.py
+++ test1.py
@@ -14,3 +14,25 @@ print(dataset.columns)
 print(dataset.columns.values)
 a = zip(dataset['square_feet'],dataset['price'])
 print(list(a))
+print(dataset)
+dataset['square_feet'].values
+#b = list(dataset['square_feet'])
+
+def split_data(dataset):
+    s=[]
+    p=[]
+    for sf,pr in zip(dataset['square_feet'],dataset['price']):
+        s.append([sf])
+        p.append(pr)
+    return s,p
+
+#b = dataset['square_feet'].values
+#c = dataset['price'].values
+#b = [1,2,3,4,5]
+#c = [7,6,4,3,8]
+
+b ,c = split_data(dataset)
+
+regr = linear_model.LinearRegression()
+regr.fit(b,c)
+print(regr.predict(700))
